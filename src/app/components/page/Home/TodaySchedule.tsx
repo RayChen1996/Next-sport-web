9d882d6bd341bb865a5c6dba3ff43b5ed9d508d2
RayChen <149983470+raychen19960102@users.noreply.github.com>
2024-04-06 19:20:11 +0800

    預計運動區塊rwd

--- src/app/components/page/Home/TodaySchedule.tsx
+++ src/app/components/page/Home/TodaySchedule.tsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function TodaySchedule() {
   return (
-    <div className=" container p-5  w-4/5 bg-white h-full flex min-w-md max-w-screen-xl m-auto">
-      <div className=" w-1/3 ">
-        <ul className=" flex justify-center flex-col h-full gap-2">
+    <div className=" container p-5 w-2/3  md:w-4/5 bg-white h-full md:flex m-auto">
+      <div className=" w-full md:w-1/3 ">
+        <ul className=" flex flex-row md:flex-col justify-between md:justify-center   h-full gap-2">
           <li>每日任務</li>
           <li>訓練菜單</li>
           <li>配對組員</li>
         </ul>
       </div>
-      <div className="w-2/3    flex flex-col items-center justify-center">
+      <div className=" bg-[#FAFAFA] w-full md:w-4/5    flex flex-col items-center justify-center">
         <h1 className=" text-2xl py-2 text-black font-bold">預計今日運動</h1>
         <span>Time to work！</span>
         <div className="py-2 flex text-black font-bold w-1/2 items-center  justify-center gap-2 ">
